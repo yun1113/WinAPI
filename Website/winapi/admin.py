3b5a3b80423ee1f43684db40c0d13a57a9384027
yun1113 <yunyun1113c@gmail.com>
2017-07-02 18:52:35 +0800

    add model admin - MalwareAPICallExcutionTracelAdmin

--- Website/winapi/admin.py
+++ Website/winapi/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Dll, API, MalwareAPICall
+from .models import Dll, API, MalwareAPICall, MalwareAPICallExcutionTrace
 
 
 class APIAdmin(admin.ModelAdmin):
@@ -12,10 +12,16 @@ class DllAdmin(admin.ModelAdmin):
     search_fields = ('name', )
 
 
-class MalwareAPICallAdmin(admin.ModelAdmin):
-    list_display = ('sha256', 'api', 'dll', 'count')
+class MalwareAPICallExcutionTracelAdmin(admin.ModelAdmin):
+    list_display = ('sha256', )
     search_fields = ('sha256', )
+
+
+class MalwareAPICallAdmin(admin.ModelAdmin):
+    list_display = ('malware_excution_trace', 'api', 'dll', 'count')
+    search_fields = ('malware_excution_trace__sha256', )
 
 admin.site.register(API, APIAdmin)
 admin.site.register(Dll, DllAdmin)
+admin.site.register(MalwareAPICallExcutionTrace, MalwareAPICallExcutionTracelAdmin)
 admin.site.register(MalwareAPICall, MalwareAPICallAdmin)
