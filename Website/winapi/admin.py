c27a304c0dd30926fa013bffc1b8a04045f5be43
yun1113 <yunyun1113c@gmail.com>
2017-07-02 18:21:42 +0800

    add MalwareAPICallAdmin

--- Website/winapi/admin.py
+++ Website/winapi/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Dll, API
+from .models import Dll, API, MalwareAPICall
 
 
 class APIAdmin(admin.ModelAdmin):
@@ -11,5 +11,11 @@ class DllAdmin(admin.ModelAdmin):
     list_display = ('name', )
     search_fields = ('name', )
 
+
+class MalwareAPICallAdmin(admin.ModelAdmin):
+    list_display = ('sha256', 'api', 'dll', 'count')
+    search_fields = ('sha256', )
+
 admin.site.register(API, APIAdmin)
 admin.site.register(Dll, DllAdmin)
+admin.site.register(MalwareAPICall, MalwareAPICallAdmin)
