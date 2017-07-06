3ecf87192d85e21553205925bb76a37ebf48574a
yun1113 <yunyun1113c@gmail.com>
2017-07-06 23:31:31 +0800

    add page which will list sample loaded the dlls or invoked apis

--- Website/website/urls.py
+++ Website/website/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from winapi.views import temp_view
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^winapi/$', temp_view),  # search
 ]
--- Website/winapi/views.py
+++ Website/winapi/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, render
+from models import API, Dll
 
-# Create your views here.
+
+def temp_view(request):
+    if request.method == 'POST':
+
+        model_name = request.POST.get('type', 'dll').encode('utf8')
+        name = request.POST.get('name', '')
+
+        if model_name == 'API':
+            data = API.objects.filter(name=name)
+        else:
+            data = Dll.objects.filter(name=name)
+
+        if data:
+            samples = data[0].malwareapicallexcutiontrace_set.all()
+            return render(request, 'index.html', {'request': request, 'model_name': model_name,
+                                                  'samples': samples, 'data': data[0]})
+
+    return render(request, 'index.html', locals())
